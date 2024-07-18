02828f94eed01c76ee524bf8c41c00be6212ed56
Vadim <VadimPokhabov@mail.ru>
2024-07-19 03:15:44 +0700

    Add models for users

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    """
+    Модель для пользователя
+    """
+
+    name = None
+    email = models.EmailField(unique=True, verbose_name="почта")
+    phone = models.CharField(max_length=20, verbose_name="телефон", **NULLABLE)
+    city = models.CharField(max_length=20, verbose_name="город", **NULLABLE)
+    image = models.ImageField(upload_to="user", verbose_name="аватар", **NULLABLE)
+    tg_chat_id = models.CharField(max_length=35, verbose_name='id чата телеграмм')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
