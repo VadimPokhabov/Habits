909839f51d88f7fa3e46ae13f8ccd0a418862ae7
Vadim <VadimPokhabov@mail.ru>
2024-07-19 03:20:42 +0700

    Add permissions for users

---
+++ users/permissons.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Проверьте, является ли пользователь владельцем объекта
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
